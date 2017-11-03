0138449f213244adc38a19030260ae500454abda
Kaung Khant Thu <kaungkhantthukkt2016@gmail.com>
2017-11-03 10:49:26 +0630

    no message

--- app/src/main/java/xyz/kkt/padc_assignment/MainActivity.java
+++ app/src/main/java/xyz/kkt/padc_assignment/MainActivity.java
@@ -1,31 +1,157 @@
 package xyz.kkt.padc_assignment;
 
+import android.Manifest;
+import android.app.SearchManager;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.StringRes;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.BindViews;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
+    @BindView(R.id.navigate_to_map)
+    Button navigateToMap;
+    @BindView(R.id.sharing_with_share_compact)
+    Button sharingWithShareCompact;
+    @BindView(R.id.making_ph_call)
+    Button makingPhCall;
+    @BindView(R.id.sending_emails)
+    Button sendingEmails;
+    @BindView(R.id.taking_pic_with_camera)
+    Button takingPicWithCamera;
+    @BindView(R.id.selecting_pic_from_storage)
+    Button selectingPicFromStorage;
+    @BindView(R.id.iv_hero)
+    ImageView ivHero;
+
+    private static final int PICK_IMAGE = 1;
+
+    private static final int REQUEST_CAMERA = 2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        ButterKnife.bind(this, this);
+
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+    }
+
+    @OnClick(R.id.fab)
+    public void clickFab() {
+        Snackbar.make(fab, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+    }
+
+    @OnClick(R.id.navigate_to_map)
+    public void clickNavigateToMap() {
+        Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+                Uri.parse("https://goo.gl/maps/FUtneKdKjw62"));
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.sharing_with_share_compact)
+    public void clickSharingWithShareCompact() {
+        ShareCompat.IntentBuilder intentBuilder = ShareCompat.IntentBuilder.from(this);
+        Intent intent = intentBuilder
+                .setType("text/html")
+                .setText("clickSharingWithShareCompact")
+                .setChooserTitle("Choose email client")
+                .createChooserIntent();
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    @OnClick(R.id.making_ph_call)
+    public void clickMakingPhCall() {
+        String ph_num = "09770909052";
+
+        String uri = "tel:" + ph_num.trim();
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse(uri));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        startActivity(intent);
+
+    }
+
+    @OnClick(R.id.sending_emails)
+    public void clickSendingEmails() {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/html");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Subject");
+        intent.putExtra(Intent.EXTRA_TEXT, "I'm email body.");
+
+        startActivity(Intent.createChooser(intent, "Send Email"));
+    }
+
+    @OnClick(R.id.taking_pic_with_camera)
+    public void clickTakingPicWithCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, REQUEST_CAMERA);
+        }
+    }
+
+    @OnClick(R.id.selecting_pic_from_storage)
+    public void clickSelectingPicFromStorage() {
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, PICK_IMAGE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK) {
+            Uri selectedImage = data.getData();
+            if (selectedImage != null) {
+                ivHero.setImageURI(selectedImage);
             }
-        });
+        }
+
+        if (requestCode == REQUEST_CAMERA && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imgBitmap = (Bitmap) extras.get("data");
+            if (imgBitmap != null) {
+                ivHero.setImageBitmap(imgBitmap);
+            }
+        }
     }
 
     @Override
